07126692047dcb593dfa110d8fec0a200d430f41
ShaQayeQ <shaghay3gh@gmail.com>
2025-01-16 15:05:29 +0100

    Get the location from CL

--- app.js
+++ app.js
@@ -1,35 +1,30 @@
 const geocode = require("./utils/geocode");
 const forcast = require("./utils/forcast");
 
-//! Callback Chaining
-// Chain multiple callbacks to do multiple things in specific order
+// Gets user input from CL
+const input = process.argv[2];
 
-geocode("Leipzig", (error, data) => {
-	console.log("GEOCODE");
-	if (error) {
-		return console.log(error);
-	}
-	// forcast(51.3397, 12.3731, (error, data) => {
-	forcast(data.latitute, data.longitute, (error, focastData) => {
-		console.log("FIRCAST");
+if (!input) {
+	console.log("Please provide a city name!");
+} else {
+	//! Callback Chaining
+	// Chain multiple callbacks to do multiple things in specific order
+
+	geocode(input, (error, data) => {
+		console.log("GEOCODE");
 		if (error) {
 			return console.log(error);
 		}
+		// forcast(51.3397, 12.3731, (error, data) => {
+		forcast(data.latitute, data.longitute, (error, focastData) => {
+			console.log("FIRCAST");
+			if (error) {
+				return console.log(error);
+			}
 
-		console.log(data.location);
-		console.log(focastData);
+			console.log(data.location);
+			console.log(focastData);
+		});
 	});
-});
+}
 
-// geocode("tehran", forcast);
-
-// //! Only 1 API request per second
-// setTimeout(() => {
-// 	forcast(geocode("leipzig"));
-// }, 1000);
-// setTimeout(() => {
-// 	geocode("Dresden", forcast);
-// }, 1000);
-// setTimeout(() => {
-// 	geocode("Berlin", forcast);
-// }, 1000);
